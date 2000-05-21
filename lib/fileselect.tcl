cf5f4b3e416d3098569fe4d222c1ba117dde89b4
Scott Lipcon <slipcon@users.sourceforge.net>
2000-05-21 21:58:03 +0000

    change to the file selection box to restore the suggested name on a directory change.  Not exactly right, but probably better than before (where the suggested name was lost on a directory change)

--- lib/fileselect.tcl
+++ lib/fileselect.tcl
@@ -65,17 +65,19 @@ proc FSBox {{purpose "Select file:"} {defaultName ""} {cmd ""} {errorHandler ""}
     global exwin
 
     regsub -all " +" $defaultName _ defaultName
+
+	set fileselect(origname) $defaultName
     if $fileselect(dotsByDefault) {
-	set fileselect(pattern) "{*,.*}"
-    } else {
+		set fileselect(pattern) "{*,.*}"
+   	} else {
         set fileselect(pattern) "*"
     }
     if ![info exists fileselect(lastDir)] {
-	if {$fileselect(home) == {}} {
-	    set fileselect(lastDir) [pwd]
-	} else {
-	    set fileselect(lastDir) $fileselect(home)
-	}
+		if {$fileselect(home) == {}} {
+		    set fileselect(lastDir) [pwd]
+		} else {
+	    	set fileselect(lastDir) $fileselect(home)
+		}
     }
 
     set w .fileSelect
@@ -86,6 +88,7 @@ proc FSBox {{purpose "Select file:"} {defaultName ""} {cmd ""} {errorHandler ""}
 	set fileselect(scroll) $w.file.sframe.scroll
 	set fileselect(direntry) $w.file.f1.direntry
 	set fileselect(entry) $w.file.f2.entry
+
 	set fileselect(ok) $w.but.ok
 	set fileselect(cancel) $w.but.quit
 	set fileselect(msg) $w.label
@@ -185,11 +188,11 @@ proc FSBox {{purpose "Select file:"} {defaultName ""} {cmd ""} {errorHandler ""}
 
     set dir [file dirname $defaultName]
     if {"$dir" == "." && [info exists fileselect(lastDir)]} {
-	set dir $fileselect(lastDir)
+		set dir $fileselect(lastDir)
     }
     if [catch {pwd} fileselect(pwd)] {
-	cd
-	set fileselect(pwd) [pwd]
+		cd
+		set fileselect(pwd) [pwd]
     }
     fileselect.cd $dir
     $fileselect(direntry) delete 0 end
@@ -209,17 +212,17 @@ proc FSBox {{purpose "Select file:"} {defaultName ""} {cmd ""} {errorHandler ""}
     # Wait for button hits if no callbacks are defined
 
     if {"$cmd" == "" && "$errorHandler" == ""} {
-	# wait for the box to be destroyed
-	tkwait variable fileselect(result)
+		# wait for the box to be destroyed
+		tkwait variable fileselect(result)
 
-	set path $fileselect(result)
-	set dir [file dirname $fileselect(result)]
-	if {$dir == "."} {
-	    set dir [pwd]
-	}
-	set fileselect(lastDir) $dir
-	fileselect.cd $fileselect(pwd)
-	return [string trimright [string trim $path] /]
+		set path $fileselect(result)
+		set dir [file dirname $fileselect(result)]
+		if {$dir == "."} {
+	    	set dir [pwd]
+		}
+		set fileselect(lastDir) $dir
+		fileselect.cd $fileselect(pwd)
+		return [string trimright [string trim $path] /]
     }
     fileselect.cd $fileselect(pwd)
     return ""
@@ -228,8 +231,8 @@ proc FSBox {{purpose "Select file:"} {defaultName ""} {cmd ""} {errorHandler ""}
 proc fileselect.cd { dir } {
     global fileselect
     if [catch {cd $dir} err] {
-	fileselect.yck $dir
-	cd
+		fileselect.yck $dir
+		cd
     }
 }
 # auxiliary button procedures
@@ -253,23 +256,23 @@ proc fileselect.list.cmd {w {state normal}} {
     global fileselect
     set seldir [$fileselect(direntry) get]
     if {[catch {glob $seldir} dir]} {
-	fileselect.yck "glob failed"
-	return
+		fileselect.yck "glob failed"
+		return
     }
     if {[llength $dir] > 1} {
-	set dir [file dirname $seldir]
-	set pat [file tail $seldir]
+		set dir [file dirname $seldir]
+		set pat [file tail $seldir]
     } else {
-	set pat $fileselect(pattern)
-	set dir [lindex $dir 0]
+		set pat $fileselect(pattern)
+		set dir [lindex $dir 0]
     }
     fileselect.ok
     update idletasks
     if [file isdirectory $dir] {
-	fileselect.getfiles $dir $pat $state
-	focus $fileselect(entry)
+		fileselect.getfiles $dir $pat $state
+		focus $fileselect(entry)
     } else {
-	fileselect.yck "not a dir"
+		fileselect.yck "not a dir"
     }
 }
 
@@ -279,48 +282,49 @@ proc fileselect.ok.cmd {w cmd errorHandler} {
     set seldir [$fileselect(direntry) get]
 
     if [string match /* $selname] {
-	set selected $selname
+		set selected $selname
     } else {
-	if [string match ~* $selname] {
-	    set selected $selname
-	} else {
-	    set selected [string trimright $seldir /]/$selname
-	}
+		if [string match ~* $selname] {
+	    	set selected $selname
+		} else {
+	    	set selected [string trimright $seldir /]/$selname
+		}
     }
 
     # some nasty file names may cause "file isdirectory" to return an error
     if [catch {file isdirectory $selected} isdir] {
-	fileselect.yck "isdirectory failed"
-	return
+		fileselect.yck "isdirectory failed"
+		return
     }
     if [catch {glob $selected} globlist] {
-	if ![file isdirectory [file dirname $selected]] {
-	    fileselect.yck "bad pathname"
-	    return
-	}
-	set globlist [list $selected]
+		if ![file isdirectory [file dirname $selected]] {
+	    	fileselect.yck "bad pathname"
+	    	return
+		}
+		set globlist [list $selected]
     }
     fileselect.ok
     update idletasks
 
     if {[llength $globlist] > 1} {
-	set dir [file dirname $selected]
-	set pat [file tail $selected]
-	fileselect.getfiles $dir $pat
-	return
+		set dir [file dirname $selected]
+		set pat [file tail $selected]
+		fileselect.getfiles $dir $pat
+		return
     } else {
-	set selected [lindex $globlist 0]
+		set selected [lindex $globlist 0]
     }
     if [file isdirectory $selected] {
-	fileselect.getfiles $selected $fileselect(pattern)
-	$fileselect(entry) delete 0 end
-	return
+		fileselect.getfiles $selected $fileselect(pattern)
+		$fileselect(entry) delete 0 end
+		$fileselect(entry) insert 0 $fileselect(origname)
+		return
     }
 
     if {$cmd != {}} {
-	$cmd $selected
+		$cmd $selected
     } else {
-	set fileselect(result) $selected
+		set fileselect(result) $selected
     }
     Exwin_Dismiss $w
 }
@@ -332,10 +336,10 @@ proc fileselect.getfiles { dir {pat *} {state normal} } {
 
     fileselect.cd $dir
     if [catch {set files [lsort [glob -nocomplain $pat]]} err] {
-	$fileselect(msg) configure -text $err
-	$fileselect(list) delete 0 end
-	update idletasks
-	return
+		$fileselect(msg) configure -text $err
+		$fileselect(list) delete 0 end
+		update idletasks
+		return
     }
     switch -- $state {
 	normal {
@@ -364,9 +368,9 @@ proc fileselect.getfiles { dir {pat *} {state normal} } {
     # build a reordered list of the files: directories are displayed first
     # and marked with a trailing "/"
     if [string compare $dir /] {
-	fileselect.putfiles $files [expr {($pat == "*") ? 1 : 0}]
+		fileselect.putfiles $files [expr {($pat == "*") ? 1 : 0}]
     } else {
-	fileselect.putfiles $files
+		fileselect.putfiles $files
     }
     fileselect.ok
 }
@@ -376,19 +380,19 @@ proc fileselect.putfiles {files {dotdot 0} } {
 
     $fileselect(list) delete 0 end
     if {$dotdot} {
-	$fileselect(list) insert end "../"
-	set dirnum 1
+		$fileselect(list) insert end "../"
+		set dirnum 1
     } else {
         set dirnum 0
     }
     foreach i $files {
 	catch {
 	    if {[file isdirectory $i]} {
-		if {"x$i" == "x."} continue
-		$fileselect(list) insert $dirnum $i/
-		incr dirnum
+			if {"x$i" == "x."} continue
+			$fileselect(list) insert $dirnum $i/
+			incr dirnum
 	    } else {
-		$fileselect(list) insert end $i
+			$fileselect(list) insert end $i
 	    }
 	}
     }
