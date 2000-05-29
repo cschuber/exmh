f55592b2c865998d1732e8bf8768651113a7cfdd
Scott Lipcon <slipcon@users.sourceforge.net>
2000-05-29 14:17:17 +0000

    fixed and extended comp.pl, some by Roman Maeder, some by me

--- misc/comp.pl
+++ misc/comp.pl
@@ -2,14 +2,16 @@
 
 # Arguments to the script:
 #   1  to
-#   2  cc
-#   3  bcc
-#   4  subject
-#   5  body file
+#   2  -c cc
+#   3  -b bcc
+#   4  -s subject
+#   5  -i body file
 #   6  org
 
 use FileHandle;
 use strict;
+
+use Getopt::Long;
 
 # set this to your normal components file
 my $comps = "/home/slipcon/Mail/components";
@@ -19,32 +21,42 @@ my $comps = "/home/slipcon/Mail/components";
 # the same value as $newcomps.
 my $newcomps = "/net/mercea/tmp/components.$<.$$";
 my $usecomps = "/tmp/components.$<.$$";
+#my $usecomps = $newcomps;
 
-(my $to, my $cc, my $bcc, my $subj, my $bodyfile, my $org) = @ARGV;
+use vars qw($to $cc $bcc $subj $bodyfile $org);
+use vars qw($haveto $havecc $havebcc $havesubj $havebodyf $haveorg);
+
+$haveto = $havecc = $havebcc = $havesubj = $havebodyf = $haveorg = 1;
+
+$to = shift @ARGV;
+GetOptions("c=s" => \$cc, "b=s" => \$bcc, "s=s" => \$subj, "i=s" => \$bodyfile);
+$org = shift @ARGV;
 
 if (!defined($to)) {
+	$haveto = 0;
 	$to = "";
-} 
-
+}
 if (!defined($cc)) {
+	$havecc = 0;
 	$cc = "";
 }
-
 if (!defined($bcc)) {
+	$havebcc = 0;
 	$bcc = "";
 }
-
 if (!defined($subj)) {
+	$havesubj = 0;
 	$subj = "";
 }
-
 if (!defined($bodyfile)) {
+	$havebodyf = 0;
 	$bodyfile = "";
 }
-
 if (!defined($org)) {
+	$haveorg = 0;
 	$org = "";
 }
+
 
 my $infh = new FileHandle;
 my $outfh = new FileHandle;
@@ -55,10 +67,37 @@ while (<$infh>) {
 	chomp;
 	if (/^To:/) {
 		print $outfh $_ . " " . $to . "\n";
+		$haveto = 0;
 	} elsif ((/^Cc:/) || (/^cc:/)) {
 		print $outfh $_ . " " . $cc . "\n";
+		$havecc = 0;
+	} elsif ((/^Bcc:/)  || (/^bcc:/)) {
+		print $outfh $_ . " " . $bcc . "\n";
+		$havebcc = 0;
+	} elsif (/^Organization:/) {
+		print $outfh $_ . " " . $org . "\n";
+		$haveorg = 0;
 	} elsif (/^Subject:/) {
 		print $outfh $_ . " " . $subj . "\n";
+		$havesubj = 0;
+	} elsif (/^--------/) {  # end of components file
+		if ($havebcc) {
+			print $outfh "Bcc: $bcc\n";
+			$havebcc = 0;
+		}
+		if ($haveorg) {
+			print $outfh "Organization: $org\n";
+			$haveorg = 0;
+		}
+		print $outfh "--------\n";
+		if ($havebodyf) {
+			my $bodyfh = new FileHandle;
+			$bodyfh->open($bodyfile);
+			while (<$bodyfh>) {
+				print $outfh $_;
+			}
+			$bodyfh->close();
+		}
 	} else {
 		print $outfh $_ . "\n";
 	}
