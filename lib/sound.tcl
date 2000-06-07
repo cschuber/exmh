de0a1363ecc0ec89215cfb7e48e96433587fe202
Scott Lipcon <slipcon@users.sourceforge.net>
2000-06-07 23:16:55 +0000

    expand ~ in sound file names, I think this is right, but I don't have a sound card in this machine.

--- lib/sound.tcl
+++ lib/sound.tcl
@@ -26,56 +26,64 @@ proc Sound_On {} {
 proc Sound_Feedback { delta } {
     global sound exmh
     if {$sound(enabled) && $sound(bell)} {
-	if [catch bell] {
-	    catch blt_bell
-	}
-	Exmh_Debug bell
-	return
+		if [catch bell] {
+	    	catch blt_bell
+		}
+		Exmh_Debug bell
+		return
     }
     if {!$sound(enabled) || ($sound(cmd) == {})} {
-	return
+		return
     }
+
+	if [string match ~* $sound(newMsg)] {
+		set sound(newMsg) [glob -nocomplain $sound(newMsg)]
+	}
+
     if ![string match /*  $sound(newMsg)] {
-	set sound(newMsg) $exmh(library)/$sound(newMsg)
+		set sound(newMsg) $exmh(library)/$sound(newMsg)
     }
     set num 0
     while {$delta > 0} {
-	incr num
-	set delta [expr $delta/4]
+		incr num
+		set delta [expr $delta/4]
     }
     set cmd [concat exec $sound(cmd)]
     if {$sound(multifile)} {
-	for {set i 0} {$i < $num} {incr i} {
-	    lappend cmd $sound(newMsg)
-	}
-	if [catch {
-	    eval $cmd &
-	} err] {
-	    Exmh_Debug "Sound_Feedback: $err"
-	}
+		for {set i 0} {$i < $num} {incr i} {
+			lappend cmd $sound(newMsg)
+		}
+		if [catch {
+	    	eval $cmd &
+		} err] {
+	    	Exmh_Debug "Sound_Feedback: $err"
+		}
     } else {
-	for {set i 0} {$i < $num} {incr i} {
-	    if [catch {
-		eval $cmd $sound(newMsg)
-	    } err] {
-		Exmh_Debug "Sound_Feedback: $err"
-	    }
-	}
+		for {set i 0} {$i < $num} {incr i} {
+		    if [catch {
+				eval $cmd $sound(newMsg)
+	    	} err] {
+				Exmh_Debug "Sound_Feedback: $err"
+	    	}
+		}
     }
 }
 
 proc Sound_Error {  } {
     global sound exmh
     if {!$sound(enabled) || ($sound(cmd) == {})} {
-	return
+		return
     }
+	if [string match ~* $sound(error)] {
+		set sound(error) [glob -nocomplain $sound(error)]
+	}
     if ![string match /*  $sound(error)] {
-	set sound(error) $exmh(library)/$sound(error)
+		set sound(error) $exmh(library)/$sound(error)
     }
     if [catch {
-	eval exec $sound(cmd) $sound(error)
+		eval exec $sound(cmd) $sound(error)
     } err] {
-	Exmh_Debug "Sound_Error: $err"
+		Exmh_Debug "Sound_Error: $err"
     }
 }
 
