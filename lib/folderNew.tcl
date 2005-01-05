256b1a5502388a51a51c138cc78949b784c2d52b
Brent B. Welch <welch@users.sourceforge.net>
2005-01-05 23:58:06 +0000

    lib/folderNew.tcl: folder-protect value wasn't being interpreted as   octal when creating new folders. exmh.install: bumped version date

--- lib/folderNew.tcl
+++ lib/folderNew.tcl
@@ -88,13 +88,13 @@ proc FolderNewCommit { top entry } {
 	if {[catch {
 	    close [open [file join $path .mh_sequences] w]
 	    if [info exists mhProfile(folder-protect)] {
-		file attributes $path/.mh_sequences -permissions $mhProfile(folder-protect)
+		file attributes $path/.mh_sequences -permissions 0$mhProfile(folder-protect)
 	    }
 	} msg]} {
 	    Exmh_Debug ".mh_sequences: $msg"
 	}
 	if [info exists mhProfile(folder-protect)] {
-	    file attributes $path -permissions $mhProfile(folder-protect)
+	    file attributes $path -permissions 0$mhProfile(folder-protect)
 	}
     }
     Exmh_Status "Created folder $name"
